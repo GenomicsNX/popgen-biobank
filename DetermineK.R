84ac06e0161f3ba52aa12278da985009f61edb1b
andrewcon <ac16225@bristol.ac.uk>
2021-12-15 17:12:54 +0000

    Added first batch to GitHub

---
+++ DetermineK.R
@@ -0,0 +1,75 @@
+#' A kmeans and Silhouette Analysis to determine the best K for your data
+#'
+#' This function estimates the best K (number of clusters) for your data set using Kmeans and a Silhouette Analysis
+#' @param mydata a data frame or matrix of data
+#' @param kmax the maximum number of k groups to estimate
+#' @param num_iter the number of iterations used in the kmeans analysis
+#' @param plotrepress default is to plot the data (FALSE), set to TRUE to repress the plot
+#' @keywords kmeans, silhouette
+#' @export
+#' @examples
+#' DetermineK()
+DetermineK = function(mydata, kmax = 20, num_iter = 100, plotrepress=FALSE){
+  ##############################
+  ## 1. Kmeans
+  ##############################
+  K = lapply(1:kmax, function(k){
+    kmeans(mydata, centers = k, iter.max = num_iter, nstart = 10 )
+    })  
+  
+  ##############################
+  ## 2. Within Group SumOfSquares
+  ##############################
+  WSS <- sapply(1:kmax,function(i){
+    K[[i]]$tot.withinss
+    })
+  
+  ##############################
+  ## 2. Silhouette analysis
+  ##############################
+  sil = sapply(2:kmax, function(k){
+    cluster::pam(mydata, k)$silinfo$avg.width
+  })
+  k.sil = which.max(sil) + 1
+  
+  ##############################
+  ## 3. Silhouette analysis 2
+  ##############################
+  sil.est = fpc::pamk(mydata, krange=1:kmax)$nc
+  
+  ##############################
+  ## 4. Plot
+  ##############################
+  if(plotrepress == 0){
+    ## PLOT LAYOUT
+    mat = matrix(c(1,2,3,3), ncol = 2, byrow = FALSE)
+    layout(mat)
+    ## Plot WSS
+    plot(1:kmax, WSS, type="b", pch = 19, frame = FALSE, 
+      xlab="Number of clusters K", 
+      ylab="Tot. Within Clusters SS", 
+      main = "Cluster Within Group\nSum of Squares")
+    abline(v = sil.est , lty =2)
+    #### PLOT Silhouette
+    plot(c(0,sil), type = "b", pch = 19, frame = FALSE, 
+      main = "Silhouette Analysis", 
+      ylab  = "Avg Silhouette Width")
+    abline(v = sil.est , lty =2)
+    
+    ### Distance Matrix
+    d <- dist(mydata, method = "euclidean")
+    plot(cluster::pam(d, sil.est))
+  }
+  # ##############################
+  # ##  estimate the clusters
+  # ##############################
+  # K = kmeans(mydata, centers = sil.est, iter.max = num_iter, nstart = 10 )$cluster
+  # ##
+  # Krequested = kmeans(mydata, centers = desiredK, iter.max = num_iter, nstart = 10 )$cluster
+  # ##############################
+  ## 5. Prepare Output
+  ##############################
+  return(list( K = K, BestK = sil.est, WSS = WSS, Silhouette = sil  ))
+  
+    
+}
